62f01dda4864a03387ea198582ca3976f0594ff9
Jonathan ƒ(n) Reed <jonathanalanreed1983@gmail.com>
2026-09-17 10:44:26 -0400

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -3,6 +3,9 @@ open Lake DSL
 
 package «NonExistenceOfPerfectCuboids» where
 
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.28.0"
+
 @[default_target]
-lean_lib «NonExistenceOfPerfectCuboids» where
-  srcDir := "."
+lean_lib «NoPerfectCuboids» where
+  srcDir := ".
